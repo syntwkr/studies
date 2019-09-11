db14476c440c4f7b110de671f0c84debbffd13fe
syntwkr <54696959+syntwkr@users.noreply.github.com>
2019-09-12 09:26:34 +1000

    Update mario.c

--- mario.c
+++ mario.c
@@ -2,34 +2,43 @@
 #include <stdio.h>
 #define SPACES 2
 
-/* draws "mario" pyramid
-	created on 10.09.2019 */
+/* Draws "mario" pyramid
+ created on 10.09.2019 */
 
 int main(void)
 {
-    int height, space, hash, line_num, position;
-    
+    int height, line_num, position;
+
     height = get_int("Height: ");
-    
-    while (!(height > 0 && height <= 10))
+
+    // Keeps asking the user to put the height until it's greater than 0 but less than 23
+    while (!(height > 0 && height <= 23))
     {
         height = get_int("Height: ");
     }
 
+    // Outer loop to "walk" through the rows
     for (line_num = 1; line_num <= height; line_num++)
     {
+        // Inner loop to draw the line of spaces and hashes
         for (position = 0; position <= height + SPACES + line_num; position++)
         {
+            // Quite a complicated condition when to draw the spaces -- first, put the new line -- second and draw the hashes -- last
             if ((position >= 0 && position < height - line_num) || (position >= height && position < height + SPACES))
+            {
                 putchar(' ');
-            //else if ((position >= height - line_num && position < height) || (position >= height && position <= height + 1 + line_num))
-                //putchar('#');
-            //else
-                //putchar('\n');
+            }
+
             else if (position == height + SPACES + line_num)
+            {
                 putchar('\n');
+            }
+
             else
+            {
                 putchar('#');
+            }
         }
     }
-}   
+
+}
