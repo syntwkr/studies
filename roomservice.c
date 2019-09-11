febd9dd05c1e22051e72fd7d65eadcda7d8e3809
syntwkr <54696959+syntwkr@users.noreply.github.com>
2019-09-11 10:42:16 +1000

    Create roomservice.c

---
+++ roomservice.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(void)
+{  
+    int floor, room, cupboard, shelf;
+    
+    for (floor = 1; floor <= 2; ++floor)
+    {
+        printf("Floor: %d\n", floor);
+        for (room = 1; room <= 3; ++room)
+        {
+            printf("\tRoom: %d\n", room);
+            for (cupboard = 1; cupboard <= 2; ++cupboard)
+            {
+                printf("\t\tCupboard: %d\n", cupboard);  
+                for (shelf = 1; shelf <= 5; ++shelf)
+                    if (shelf>=2 && shelf<=4)
+                    {
+                        printf("\t\t\tShelf: %d\n", shelf);
+                    }
+                    
+            }
+        }
+    }
+    return 0;
+}
